9e9f400d7d33a00e11569fc59385af67af74403a
eoinkane <eoinkane23@gmail.com>
2021-03-13 18:53:43 +0000

    #2 #3 getSalariesByDepartment method

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -291,5 +291,40 @@ public class App
             return null;
         }
     }
+
+    public ArrayList<Employee> getSalariesByDepartment(Department dept) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                    + "FROM employees, salaries, dept_emp, departments "
+                    + "WHERE employees.emp_no = salaries.emp_no "
+                    + "AND employees.emp_no = dept_emp.emp_no "
+                    + "AND dept_emp.dept_no = departments.dept_no "
+                    + "AND salaries.to_date = '9999-01-01' "
+                    + "AND departments.dept_no = '" + dept.dept_no + "' "
+                    + "ORDER BY employees.emp_no ASC ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<>();
+            while (rset.next()) {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salaries by role");
+            return null;
+        }
+    }
 }
 
